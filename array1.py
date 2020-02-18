d948939f2b6ab2c5a12506b489fa681dde458ec5
mourice-oduor <otienomaurice364@gmail.com>
2020-02-18 12:01:41 -0500

    arrays implementation

---
+++ array1.py
@@ -0,0 +1,15 @@
+#Arrays in python presented in form of lists..
+numbers = [10,20,30,40,50]
+print(numbers)
+
+#iterating through
+'''for num in numbers:
+    print(num)
+'''
+#find max val in array/search
+max = numbers[4]
+for num in numbers:
+    if num > max:
+        max == num
+        
+print(max)
